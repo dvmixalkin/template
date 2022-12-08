f15153b3a0dbe88c8f452dff8c337da57e0e6a1e
dvmixalkin <dvmixalkin@gmail.com>
2022-12-08 11:25:26 +0300

    add putText

--- deaw_rectangle.py
+++ deaw_rectangle.py
@@ -5,18 +5,18 @@ from PIL import Image
 
 def draw_rectangle():
     # Reading an image in default mode
-    image = np.zeros([300, 300, 3])
+    image = np.ones([300, 300, 3])*255
 
     # Window name in which image is displayed
     window_name = 'Image'
 
     # Start coordinate, here (5, 5)
     # represents the top left corner of rectangle
-    start_point = (5, 5)
+    x_min, y_min = 45, 45
 
     # Ending coordinate, here (220, 220)
     # represents the bottom right corner of rectangle
-    end_point = (220, 220)
+    x_max, y_max = 220, 220
 
     # Blue color in BGR
     color = (255, 0, 0)
@@ -26,8 +26,9 @@ def draw_rectangle():
 
     # Using cv2.rectangle() method
     # Draw a rectangle with blue line borders of thickness of 2 px
-    image = cv2.rectangle(image, start_point, end_point, color, thickness)
-
+    image = cv2.rectangle(image, [x_min, y_min], [x_max, y_max], color, thickness)
+    'fontScale: Font scale factor that is multiplied by the font-specific base size.'
+    cv2.putText(image, 'Fedex', (x_min, y_min - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
     # Displaying the image
     Image.fromarray(image.astype(np.uint8)).show()
 
